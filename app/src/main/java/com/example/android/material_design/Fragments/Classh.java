619d3ecf73e2e2514a47daf7fbb8ecc9fc530f43
Ekta Nijhawan <ektanijhawan1@gmail.com>
2016-08-31 04:30:53 +0530

    movie detail done

---
+++ app/src/main/java/com/example/android/material_design/Fragments/Classh.java
@@ -0,0 +1,16 @@
+package com.example.android.material_design.Fragments;
+
+/**
+ * Created by Ekta on 31-08-2016.
+ */
+public class Classh {
+    String title;
+    public void setData(String t)
+    {
+        title=t;
+    }
+    public String getData()
+    {
+        return  title;
+    }
+}
